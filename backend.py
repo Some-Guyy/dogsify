5002dc0dee7003d238e5d7174f34b09219bc3e6c
Some Guy <ozruen@gmail.com>
2019-06-10 00:23:05 +0800

    Adding to repo

---
+++ backend.py
@@ -0,0 +1,96 @@
+from flask import request, jsonify, Flask
+
+import base64
+import numpy as np
+import io
+import os
+
+import keras
+from keras import backend as K
+from keras.models import Sequential, load_model
+from keras.preprocessing.image import ImageDataGenerator, img_to_array
+from keras.utils.generic_utils import CustomObjectScope
+
+import tensorflow as tf
+
+from PIL import Image as view_image
+
+app = Flask(__name__)
+
+def get_model():
+    global model
+    model_path = 'models/120_dog_breeds_classifier_v2.h5'
+
+    with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D}):
+        model = load_model(model_path)
+
+    global graph
+    graph = tf.get_default_graph()
+    print("[INFO] Model loaded!")
+
+def prepare_image(image, target_size):
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(target_size)
+    img_array = img_to_array(image)
+    img_array_expanded_dims = np.expand_dims(img_array, axis = 0)
+    return keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
+
+print("[INFO] Loading Keras model...")
+get_model()
+
+predict_count = 0
+
+@app.route("/predict", methods = ['POST'])
+def predict():
+    print('[INFO] Loading method...')
+    num_of_result = 6
+    message = request.get_json(force = True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    image = view_image.open(io.BytesIO(decoded))
+    preprocessed_image = prepare_image(image, target_size=(224, 224))
+
+    with graph.as_default():
+        prediction = model.predict(preprocessed_image)
+    
+    result_dictionary = {}
+    class_list = []
+    class_index_count = 0
+    print_count = 0
+
+    class_array = []
+    percentage_array = []
+
+    global predict_count
+
+    print('[INFO] Loading classes...')
+    for folder in os.listdir('classes/120-dog-breeds/'):
+        class_list.append(folder)
+
+    for i in class_list:
+        result_dictionary[i] = prediction[0][class_index_count]
+        class_index_count = class_index_count + 1
+
+    if num_of_result == 0:
+        num_of_result = len(result_dictionary)
+
+    print('[INFO] Appending arrays...')
+    for key, value in sorted(result_dictionary.items(), key=lambda item: item[1], reverse = True):
+
+        if print_count < num_of_result:
+            class_array.append(key[10:])
+            percentage_array.append(str(np.around(value*100, decimals = 2)) + "%")
+            print_count = print_count + 1
+
+    response = {
+        'prediction': {
+            'class': class_array,
+            'percentage': percentage_array
+        }
+    }
+
+    predict_count = predict_count + 1
+    print("Number of predicts so far:", predict_count)
+
+    return jsonify(response)
---
+++ static/index.js
@@ -0,0 +1,98 @@
+let base64Image;
+$('#image-selector').change(function () {
+    let reader = new FileReader();
+
+    reader.onload = function(e) {
+        let dataURL = reader.result;
+        $('#selected-image').attr('src', dataURL);
+        $('#predicted-image').attr('src', '');
+        dataURLnoPNG = dataURL.replace('data:image/png;base64,', '');
+        dataURLFinal = dataURLnoPNG.replace('data:image/jpeg;base64,', '');
+        base64Image = dataURLFinal
+    }
+
+    reader.readAsDataURL($('#image-selector')[0].files[0]);
+    $('#predict-pretext').empty();
+    $('#predict-button').empty();
+
+    $('#predicted-dog-label').empty();
+    $('#predicted-dog-label').append('Predicted Dog');
+
+    $('#predict-pretext').append('Now...predict the breed!');
+    $('#predict-button').append('<button type="button" class="btn btn-outline-leaves">Predict</button>');
+
+    $('#main-prediction').empty();
+    $('#other-predictions').empty();
+});
+
+$('#predict-button').click(function(event) {
+    $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+
+    $('#main-prediction').append('Predicting the image...');
+    
+    let message = {
+        image: base64Image
+    }
+
+    $.post('http://192.168.1.64:5000/predict', JSON.stringify(message), function(response) {
+        $('#main-prediction').empty();
+        $('#other-predictions').empty();
+
+        if (['a', 'e', 'i', 'o', 'u'].indexOf(response.prediction.class[0].charAt(0).toLowerCase()) !== -1) {
+            $('#main-prediction').append('Is that an...' + response.prediction.class[0] + '?<br><span style="color: #c5001a;">' + response.prediction.percentage[0] + '</span> Match!');
+        } else {
+            $('#main-prediction').append('Is that a...' + response.prediction.class[0] + '?<br><span style="color: #c5001a;">' + response.prediction.percentage[0] + '</span> Match!');
+        }
+
+        if (parseFloat(response.prediction.percentage[0].slice(0, -1)) > 33) {
+            $('#main-prediction').empty();
+            if (['a', 'e', 'i', 'o', 'u'].indexOf(response.prediction.class[0].charAt(0).toLowerCase()) !== -1) {
+                $('#main-prediction').append('Is that an...' + response.prediction.class[0] + '?<br><span style="color: #ffbb00;">' + response.prediction.percentage[0] + '</span> Match!');
+            } else {
+                $('#main-prediction').append('Is that a...' + response.prediction.class[0] + '?<br><span style="color: #ffbb00;">' + response.prediction.percentage[0] + '</span> Match!');
+            }
+        }
+
+        if (parseFloat(response.prediction.percentage[0].slice(0, -1)) > 66) {
+            $('#main-prediction').empty();
+            if (['a', 'e', 'i', 'o', 'u'].indexOf(response.prediction.class[0].charAt(0).toLowerCase()) !== -1) {
+                $('#main-prediction').append('Is that an...' + response.prediction.class[0] + '?<br><span style="color: #28a745;">' + response.prediction.percentage[0] + '</span> Match!');
+            } else {
+                $('#main-prediction').append('Is that a...' + response.prediction.class[0] + '?<br><span style="color: #28a745;">' + response.prediction.percentage[0] + '</span> Match!');
+            }
+        }
+
+        $('#predicted-dog-label').empty();
+        $('#predicted-dog-label').append(response.prediction.class[0]);
+
+        $('#predicted-image').attr('src', 'assets/dog_images/' + response.prediction.class[0] + '.jpg');
+        $('#other-predictions').append('<u>Other Predictions:</u> ' + '<br>');
+
+        for (var i = 1; i < response.prediction.class.length; i++) {
+            $('#other-predictions').append(response.prediction.class[i] + ': ' + response.prediction.percentage[i] + '<br>')
+        }
+    });
+});
+
+$('#credits-link').click(function(event) {
+    $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+
+    $('#credits-section').empty();
+    $('#credits-section').append(
+    '<div class="col">\
+        <div class="card backColorBark colorMarble">\
+            <div class="card-body">\
+                <h3 class="card-title">Credits:</h3>\
+                <p class="card-text">\
+                    Made by: Some random guy who thought that this was a good idea.<br>\
+                    Model used for transfer learning: <a href="https://keras.io/applications/#mobilenet">MobileNet</a><br>\
+                    Libraries used: <a href="https://keras.io/">Keras</a> and <a href="https://www.tensorflow.org/">TensorFlow</a><br>\
+                    Dataset used to train model and images for sample predicted dogs: <a href="https://www.kaggle.com/jessicali9530/stanford-dogs-dataset">Stanford Dogs Dataset</a><br>\
+                    Have feedback to give? Email me at 1705408a@student.tp.edu.sg<br>\
+                    Hope you enjoyed this little web application!\
+                </p><br>\
+            </div>\
+        </div>\
+    </div>'
+    );
+});
